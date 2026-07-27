bef65bf12d38fd6f2acab3067f3ec42f0efc5a07
shukla gaurav <shuklaagaurav29@gmail.com>
2026-07-27 16:14:48 +0530

    search in binary search trees

---
+++ binary_search_trees/search_in_bst.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool search(Node* root, int key) {
+     if(root == NULL) return false;
+     
+     if(root->data == key) return true;
+     
+     if(key > root->data){
+         return search(root->right , key);
+     }
+     else return search(root->left , key);
+    }
+};
