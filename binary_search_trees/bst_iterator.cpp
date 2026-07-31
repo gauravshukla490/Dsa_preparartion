03fcf461566e85bbf6187767485e459296c0238a
shukla gaurav <shuklaagaurav29@gmail.com>
2026-07-31 07:34:05 +0530

    bst interator

---
+++ binary_search_trees/bst_iterator.cpp
@@ -0,0 +1,26 @@
+class BSTIterator {
+     stack<TreeNode*>st;
+public:
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        pushAll(temp->right);
+        return temp->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+
+    private:
+       void pushAll(TreeNode* node){
+           while(node!=NULL){
+            st.push(node);
+            node = node->left;
+           }
+       }
+};
