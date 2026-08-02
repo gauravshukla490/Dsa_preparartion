2040c361abff673782424d98eec36e8a51edc28f
shukla gaurav <shuklaagaurav29@gmail.com>
2026-08-02 05:53:16 +0530

    correction in heap sort loop

--- heaps/heap_sort.cpp
+++ heaps/heap_sort.cpp
@@ -22,7 +22,7 @@ class Solution {
     void heapSort(vector<int>& arr) {
         int n = arr.size();
         
-        for(int i =(n-1)/2 ; i>=0 ;i--){
+        for(int i =n/2 - 1 ; i>=0 ;i--){
             heapify(arr , n , i);
         }
         
