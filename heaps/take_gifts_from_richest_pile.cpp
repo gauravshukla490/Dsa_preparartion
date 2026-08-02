d70aa2067990744981e13f6f6292a80327755da9
shukla gaurav <shuklaagaurav29@gmail.com>
2026-08-02 08:34:47 +0530

    take gifts from richest pile

---
+++ heaps/take_gifts_from_richest_pile.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        if(gifts.size()==0) return 0;
+
+        priority_queue<long long>p;
+
+        for(int i = 0 ;i < gifts.size() ;i++){
+            p.push(gifts[i]);
+        }
+
+        while(k--){
+            long long gift = p.top();
+            p.pop();
+
+            p.push(sqrt(gift));
+        }
+        long long ans = 0 ;
+        while(!p.empty()){
+              ans +=  p.top();
+              p.pop();
+        }
+
+        return ans;
+    }
+};
